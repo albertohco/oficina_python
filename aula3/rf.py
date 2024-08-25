14d5a31f5188eb2f685bd4e4bab10f44a7ce6c79
Alberto Henrique Cordeiro <acordeiro@3anet.com.br>
2024-08-25 20:27:54 -0300

    inclusão arquivos do prof. Ramalho aula 3

--- aula3/rf.py
+++ aula3/rf.py
@@ -2,16 +2,29 @@
 
 '''
 #Comandos para fazer testes:
-python -m doctest rf.py -v
+#python -m doctest rf.py -v
 
 >>> main([])
-Please provide words to search,
+Please provide words to search.
 
 >>> main(['cruzeiro']) # doctest: +NORMALIZE_WHITESPACE
 U+20A2 N{CRUZEIRO SIGN} CRUZEIRO SIGN
 
->>> search(['cat'])
-U+20A2 N{CRUZEIRO SIGN} CRUZEIRO SIGN
+>>> search(['cat'])  # doctest: +NORMALIZE_WHITESPACE
+(41654, 'YI SYLLABLE CAT')
+(52289, 'HANGUL SYLLABLE CAT')
+(66028, 'PHAISTOS DISC SIGN CAT')
+(128008, 'CAT')
+(128049, 'CAT FACE')
+(128568, 'GRINNING CAT FACE WITH SMILING EYES')
+(128569, 'CAT FACE WITH TEARS OF JOY')
+(128570, 'SMILING CAT FACE WITH OPEN MOUTH')
+(128571, 'SMILING CAT FACE WITH HEART-SHAPED EYES')
+(128572, 'CAT FACE WITH WRY SMILE')
+(128573, 'KISSING CAT FACE WITH CLOSED EYES')
+(128574, 'POUTING CAT FACE')
+(128575, 'CRYING CAT FACE')
+(128576, 'WEARY CAT FACE')
 
 '''
 import sys
@@ -34,7 +47,7 @@ def main(args: list[str]) -> None:
     if args:
         search(args)
     else:
-        print('Please provide words to search, ')
+        print('Please provide words to search.')
 
 if __name__ == '__main__':
     import sys
